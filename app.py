3084ef6f7f56f61fd90d535acf4efeeedc7a30c0
Yansheng Liu <yansheng.liu2000@gmail.com>
2018-08-02 10:46:32 +1000

    again test for google cloud

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
 
 app = Flask(__name__)
-
+manager=Manager(app)
 
 @app.route('/')
 def hello_world():
-    return 'This is google cloud!'
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
